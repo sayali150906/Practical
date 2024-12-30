d4d61a875b086a540d6a1ad2fb9057d30dd12fbd
sayali150906 <chimbalkarsayali@gmail.com>
2024-12-30 12:44:48 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
   public static void main(String args[]){
-    System.out.println("Welcome to CI/CD pipeline");
+    System.out.println("Welcome to CI/CD pipeline practical");
   }
 }
